45e3dc164754674e8bcc736c773f82c07b4294dc
melvin pierre <melvin.pierre.mp@gmail.com>
2025-04-29 11:01:52 +0200

    Start test reset password

---
+++ tests/Functional/Api/ResetTokenTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Api;
+
+use App\Entity\User;
+use App\Factory\UserFactory;
+use App\Tests\Functional\Api\AbstractTestCase;
+
+final class ResetTokenTest extends AbstractTestCase
+{
+    private const string ROUTE = '/reset-password';
+
+    public function testResetPassword(): void
+    {
+        UserFactory::CreateMany(48);
+
+        $user = UserFactory::new()->create(['roles' => ['ROLE_ADMIN']]);
+
+        $response = $this->createClientWithCredentials($user)->request('GET', self::ROUTE,
+            [
+                'headers' => [
+                    'Accept' => 'application/ld+json',
+                ],
+            ]
+        );
+
+        $token = $response['reset_token'];
+
+        if ($token) {
+            $response = $this->createClientWithCredentials($user)->request('GET', self::ROUTE.'/reset/'.$token,
+                [
+                    'headers' => [
+                        'Accept' => 'application/ld+json',
+                    ],
+                ]
+            );
+
+            if ($response['success']) {
+                $this->assertResponseIsSuccessful();
+                $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+            }
+        }
+        $this->assertResponseStatusCodeSame(403);
+    }
+}
